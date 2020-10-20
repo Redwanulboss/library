f69de7728b0c20d92ab938fb4286e1d86199d66c
Redwanulboss <73173236+Redwanulboss@users.noreply.github.com>
2020-10-20 21:14:39 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,190 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if($_SESSION['login']!=''){
+$_SESSION['login']='';
+}
+if(isset($_POST['login']))
+{
+  //code for captach verification
+if ($_POST["vercode"] != $_SESSION["vercode"] OR $_SESSION["vercode"]=='')  {
+        echo "<script>alert('Incorrect verification code');</script>" ;
+    } 
+        else {
+$email=$_POST['emailid'];
+$password=md5($_POST['password']);
+$sql ="SELECT EmailId,Password,StudentId,Status FROM tblstudents WHERE EmailId=:email and Password=:password";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':email', $email, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+
+if($query->rowCount() > 0)
+{
+ foreach ($results as $result) {
+ $_SESSION['stdid']=$result->StudentId;
+if($result->Status==1)
+{
+$_SESSION['login']=$_POST['emailid'];
+echo "<script type='text/javascript'> document.location ='dashboard.php'; </script>";
+} else {
+echo "<script>alert('Your Account Has been blocked .Please contact admin');</script>";
+
+}
+}
+
+} 
+
+else{
+echo "<script>alert('Invalid Details');</script>";
+}
+}
+}
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Chittagong | Library</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME STYLE  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+    <link href="assets/js/js/bootstrap.js" rel="stylesheet" />
+
+
+    <style>
+    .row{
+      display:flex
+      margin: auto;
+  width: 50%;
+ align :center;
+  padding: 10px; 
+    }
+
+    </style>
+
+
+
+</head>
+<body>
+
+
+<div class="container">
+
+
+<center>
+
+
+
+<div>
+    <img src="assets/img/logo1.png" alt="" style="margin-top:25px;">
+</div>
+
+
+
+
+<div class="navbar row ">
+<nav class="navbar navbar-expand-lg navbar-light bg-light mr-5">
+  <a class="navbar-brand" href="#">Chittagong | Library</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active mx-4 " >
+        <a class="nav-link" href="index.php">Home <span class="sr-only ">(current)</span></a>
+      </li>
+      <li class="nav-item mx-4" >
+        <a class="nav-link" href="login.php">Account</a>
+      </li>
+      <li class="nav-item mx-4">
+        <a class="nav-link" href="#">Book</a>
+      </li>
+      <li class="nav-item mx-4">
+        <a class="nav-link " href="#about">About</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</div>
+
+</center>
+
+<div>
+
+
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/img/pic1.jpg" class="d-block w-100" height="600" width="400px">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/pic2.jpeg" class="d-block w-100" height="600px" width="400px">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/pic3.jpg" class="d-block w-100" height="600px" width="400px">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+  </div>
+
+
+
+
+
+ 
+    </div>
+    </div>
+
+    
+
+<!-- CONTENT-WRAPPER SECTION END-->
+  <?php include('includes/footer.php');?>
+
+
+ </div>
+
+ 
+      <!-- FOOTER SECTION END-->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="assets/js/js/bootstrap.js"></script>
+      <!-- CUSTOM SCRIPTS  -->
+    <script src="assets/js/custom.js"></script>
+
+</body>
+</html>
